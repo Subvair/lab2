35ace91a14742228cb3417be49702d6a287203d2
subvair <vadimogienko42k@gmail.com>
2025-02-27 00:55:26 +0700

    Создал базовый класс

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace project {
+
+    //Базовый класс
+    class Document {
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string FilePath { get; set; }
+
+    public Document(string name, string author, string filePath) {
+        Name = name;
+        Author = author;
+        FilePath = filePath;
+    }
+
+    public virtual void DisplayInfo() {
+        Console.WriteLine($"Документ: {Name}, Автор: {Author}, Путь: {FilePath}");
+    }
+}
+
+    class Program {
+
+        static void Main() {
+            
+        }
+
+    }
+
+}
